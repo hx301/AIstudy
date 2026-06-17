27a1f187fe87f6302ef145e148c191a039f02dc8
Zhanghao <zh2962940155@gmail.com>
2026-06-17 18:41:50 +0800

    2.1字面量

---
+++ python基础‘/自面量/字面量.py
@@ -0,0 +1,23 @@
+from traceback import print_tb
+
+123
+0
+-45
+2+3j
+
+'helloworld'
+'helloworld'
+'''
+第一行
+第二行
+'''
+True
+False
+None
+
+print(123)
+print('nihao')
+print('nihao')
+print("FALSE")
+print('FALSE')
+print('FALSE')
