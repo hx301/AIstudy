38601c9626a9aec235e895eec65a44c0a897ac6d
Zhanghao <zh2962940155@gmail.com>
2026-06-17 19:20:59 +0800

    2.1字面量

---
+++ python基础‘/2_注释/注释.py
@@ -0,0 +1,23 @@
+# print('hello world')  #临时屏蔽代码
+print('hello world')
+print('hello world')#单行注释
+
+print('hello world')
+
+
+"""
+多行注释
+第一行
+第二行
+第三行
+"""
+
+def add(a,b):
+    """
+    功能：计算两个数的和
+    :param a: 第一个加数
+    :param b: 第二个加数
+    :return: 返回a+b的结果
+    """
+    return a+b
+help(add)
